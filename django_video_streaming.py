36d65182f604d1762d05ff97fd8486494c04f058
cl0wnfortalk <160296915+cl0wnfortalk@users.noreply.github.com>
2025-04-06 03:47:19 +0400

    Update django_video_streaming.py

--- django_video_streaming.py
+++ django_video_streaming.py
@@ -4,16 +4,12 @@ def watch(request, slug):
     video = get_object_or_404(Video, slug=slug)
     video_file = video.video.open()
 
-    # Получаем заголовок "Range" из запроса
     range_header = request.META.get('HTTP_RANGE', None)
     if not range_header:
-        # Если диапазон не указан, отправляем весь файл
         response = HttpResponse(video_file.read(), content_type='video/mp4')
         response['Content-Disposition'] = f'inline; filename="{video.title}"'
         return response
 
-    # Обрабатываем запрос диапазона
-    # Пример: "bytes=0-"
     range_match = re.match(r'bytes=(\d+)-(\d*)', range_header)
     if not range_match:
         return HttpResponse(status=416)  # Неподдерживаемый диапазон
@@ -27,10 +23,9 @@ def watch(request, slug):
         end = int(range_match.group(2))
 
 
-    # Получаем длину файла
-    video_file.seek(0, 2)  # Переходим в конец файла
+    video_file.seek(0, 2)  # в конец файла
     file_size = video_file.tell()
-    video_file.seek(0)  # Возвращаемся в начало файла
+    video_file.seek(0)  # в начало файла
 
     if end is None or end >= file_size:
         end = file_size - 1
@@ -38,9 +33,8 @@ def watch(request, slug):
     length = end - start + 1
     video_file.seek(start)
 
-    # Создаем ответ с заголовками для поддержки диапазонов
     response = HttpResponse(video_file.read(length), content_type='video/mp4')
     response['Content-Disposition'] = f'inline; filename="{video.title}"'
-    response.status_code = 206  # Частичный ответ
+    response.status_code = 206  
     response['Content-Range'] = f'bytes {start}-{end}/{file_size}'
     return response
